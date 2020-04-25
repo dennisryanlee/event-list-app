2d836b743acaa8cb28a91f1df8d893f601fa76eb
dennisryanlee <dennisryanlee@gmail.com>
2020-04-25 15:16:52 -0400

    store updated

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,18 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
+  modules: {},
   mutations: {},
   actions: {},
-  modules: {},
 });
