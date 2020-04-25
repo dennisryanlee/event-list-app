5d986ddbc29a7a469bcf1c22ce570c65c0b0b988
dennisryanlee <dennisryanlee@gmail.com>
2020-04-25 15:17:16 -0400

    getter for user name

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>Create Event</h1>
+  <h1>Create Event, {{ userName }}</h1>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 @Component
-class EventCreate extends Vue {}
+class EventCreate extends Vue {
+  get userName() {
+    return this.$store.state.user.name;
+  }
+}
 
 export default EventCreate;
 </script>
